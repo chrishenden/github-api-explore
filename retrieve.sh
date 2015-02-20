4b6c80dd62a00930d888bdb6f02ef30229de993d
Chris Henden <chris@recurrence.co.uk>
2015-02-20 20:28:39 +0000

    Actually work

--- retrieve.sh
+++ retrieve.sh
@@ -1,1 +1,8 @@
-curl -u <token>:x-oauth-basic https://api.github.com/orgs/BBC-Data/members
+curl -u <token>:x-oauth-basic -o data/members1.json https://api.github.com/orgs/BBC-Data/members?page=1
+curl -u <token>:x-oauth-basic -o data/members2.json https://api.github.com/orgs/BBC-Data/members?page=2
+curl -u <token>:x-oauth-basic -o data/members3.json https://api.github.com/orgs/BBC-Data/members?page=3
+curl -u <token>:x-oauth-basic -o data/members4.json https://api.github.com/orgs/BBC-Data/members?page=4
+curl -u <token>:x-oauth-basic -o data/members5.json https://api.github.com/orgs/BBC-Data/members?page=5
+curl -u <token>:x-oauth-basic -o data/members6.json https://api.github.com/orgs/BBC-Data/members?page=6
+curl -u <token>:x-oauth-basic -o data/members7.json https://api.github.com/orgs/BBC-Data/members?page=7
+curl -u <token>:x-oauth-basic -o data/members8.json https://api.github.com/orgs/BBC-Data/members?page=8
