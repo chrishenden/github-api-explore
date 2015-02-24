c12b9292b1f59831b1b463cd6ebc99dda9141513
Chris Henden <chris@recurrence.co.uk>
2015-02-24 11:00:46 +0000

    Turns out no authentication needed. Hmmm.

--- retrieve.sh
+++ retrieve.sh
@@ -1,8 +1,8 @@
-curl -u <token>:x-oauth-basic -o data/members1.json https://api.github.com/orgs/BBC-Data/members?page=1
-curl -u <token>:x-oauth-basic -o data/members2.json https://api.github.com/orgs/BBC-Data/members?page=2
-curl -u <token>:x-oauth-basic -o data/members3.json https://api.github.com/orgs/BBC-Data/members?page=3
-curl -u <token>:x-oauth-basic -o data/members4.json https://api.github.com/orgs/BBC-Data/members?page=4
-curl -u <token>:x-oauth-basic -o data/members5.json https://api.github.com/orgs/BBC-Data/members?page=5
-curl -u <token>:x-oauth-basic -o data/members6.json https://api.github.com/orgs/BBC-Data/members?page=6
-curl -u <token>:x-oauth-basic -o data/members7.json https://api.github.com/orgs/BBC-Data/members?page=7
-curl -u <token>:x-oauth-basic -o data/members8.json https://api.github.com/orgs/BBC-Data/members?page=8
+curl -o data/members1.json https://api.github.com/orgs/BBC-Data/members?page=1
+curl -o data/members2.json https://api.github.com/orgs/BBC-Data/members?page=2
+curl -o data/members3.json https://api.github.com/orgs/BBC-Data/members?page=3
+curl -o data/members4.json https://api.github.com/orgs/BBC-Data/members?page=4
+curl -o data/members5.json https://api.github.com/orgs/BBC-Data/members?page=5
+curl -o data/members6.json https://api.github.com/orgs/BBC-Data/members?page=6
+curl -o data/members7.json https://api.github.com/orgs/BBC-Data/members?page=7
+curl -o data/members8.json https://api.github.com/orgs/BBC-Data/members?page=8
